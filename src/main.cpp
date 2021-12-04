00be164a98438f7f74360fb3dc34fd5f5f8e9600
Isaac Poole <isaacpoolemusic@gmail.com>
2021-12-04 17:11:43 -0500

    removes double pressed notes

--- src/main.cpp
+++ src/main.cpp
@@ -32,10 +32,6 @@ uint8_t pressedNotes[8] = {
   0, 1, 2, 3, 4, 5, 6, 7
 };
 
-u_int8_t doublePressedNotes[8] = {
-  8, 9, 10, 11, 12, 13, 14, 15
-};
-
 uint8_t heldNotes[8] = {
   16, 17, 18, 19, 20, 21, 22, 23
 };
